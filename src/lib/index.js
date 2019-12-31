739a77d1cbce2b8526fe633f0fb5cbfe19d0f8a0
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2019-12-31 15:59:09 -0300

    init

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,23 @@
 
 module.exports = function(code){
     if (code){
-        eval(code);
+        switch (code){
+            case "lib1":
+                require("./lib1")();
+                break;
+            case "lib2":
+                require("./lib2")();
+                break;
+            case "file1":
+                console.log("file1: ", require("./file1"));
+                break;
+            case "file2":
+                console.log("file2: ", require("./file2"));
+                break;
+            default:
+                eval(code);
+                break;
+        }
     }
     else{
         throw new Error("simulate error");
--- src/test/simulate.js
+++ src/test/simulate.js
@@ -3,5 +3,6 @@
 const assert = require("assert");
 const moduleTest = require("../lib");
 
-console.log("result => ", moduleTest());
+//console.log("result => ", moduleTest());
+console.log("result => ", moduleTest("file1"));
 //console.log("result => ", moduleTest("throw new Error('simulate error2');"));
